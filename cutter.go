3afbade3d9c4e7cfe5ebc1310294f95b392f26d2
Olivier Amblet <olivier@amblet.net>
2014-03-21 13:59:08 +0100

    Improved documentation

--- cutter.go
+++ cutter.go
@@ -1,5 +1,44 @@
 /*
 Package cutter provides a function to crop image.
+
+By default, the original image will be cropped at the
+given size from the top left corner.
+
+		croppedImg, err := cutter.Crop(img, cutter.Config{
+		  Width:  250,
+		  Height: 500,
+		})
+
+It is possible to specify the top left position:
+
+		croppedImg, err := cutter.Crop(img, cutter.Config{
+		  Width:  250,
+		  Height: 500,
+		  Anchor: image.Point{100, 100},
+		  Mode:   TopLeft, // optional, default value
+		})
+
+The Anchor property can represents the center of the cropped image
+instead of the top left corner:
+
+
+		croppedImg, err := cutter.Crop(img, cutter.Config{
+		  Width: 250,
+		  Height: 500,
+		  Mode: Centered,
+		})
+
+The default crop use the specified dimension, but it is possible
+to use Width and Heigth as a ratio instead. In this case,
+the resulting image will be as big as possible to fit the asked ratio
+from the anchor position.
+
+		croppedImg, err := cutter.Crop(baseImage, cutter.Config{
+		  Width: 4,
+		  Height: 3,
+		  Mode: Centered,
+		  Options: Ratio,
+		})
 */
 package cutter
 
