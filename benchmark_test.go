501fcc30161afc4b28f8941ce4391640c16a87f4
Olivier Amblet <olivier@amblet.net>
2014-03-23 00:21:37 +0100

    Fixed benchmark test

--- benchmark_test.go
+++ benchmark_test.go
@@ -8,9 +8,7 @@ import (
 /*
 BenchmarkCrop is used to track the Crop with sharing memory.
 
-On my laptop, the required time is lower than 0.00 ns/op.
-With a bigger, size it would probably not increase as
-nothing is copied.
+Result on my laptop: 2000000		948 ns/op
 */
 func BenchmarkCrop(b *testing.B) {
 	img := getImage()
@@ -22,8 +20,9 @@ func BenchmarkCrop(b *testing.B) {
 		Anchor: image.Point{100, 100},
 	}
 	b.ResetTimer()
-	r, _ := Crop(img, c)
-	r.Bounds()
+	for i := 0; i < b.N; i++ {
+		Crop(img, c)
+	}
 }
 
 /*
@@ -32,11 +31,11 @@ BenchmarkCropCopy is used to track the Crop with copy performance.
 Below are the actual result on my laptop given each
 optimization suggested by Nigel Tao: https://groups.google.com/forum/#!topic/golang-nuts/qxSpOOp1QOk
 
-1. initial time on my Laptop: 23 ns/op
-2. after inverting x and y in copy loop: 0.09 ns/op
-3. after removing useless call to ColorModel().Convert(): 0.08 ns/op
+1. initial time on my Laptop: 														10	 210332414 ns/op
+2. after inverting x and y in copy loop:                  10	 195377177 ns/op
+3. after removing useless call to ColorModel().Convert(): 10	 193589075 ns/op
 4. after replacing the two 'pixel' loops by a call to draw.Draw
-   to obtains the cropped image: 0.04 ns/op
+   to obtains the cropped image:													20	  84960510 ns/op
 */
 func BenchmarkCropCopy(b *testing.B) {
 	img := getImage()
@@ -49,6 +48,7 @@ func BenchmarkCropCopy(b *testing.B) {
 		Options: Copy,
 	}
 	b.ResetTimer()
-	r, _ := Crop(img, c)
-	r.Bounds()
+	for i := 0; i < b.N; i++ {
+		Crop(img, c)
+	}
 }
